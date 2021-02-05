f5b61f6a77951b8cdcefc9c73597fb2bc1bdd617
Alex Debril <alexdebril@users.noreply.github.com>
2021-02-05 17:41:27 +0100

    POST HTTP Handler

---
+++ handler/handler.go
@@ -0,0 +1,9 @@
+package handler
+
+import "net/http"
+
+type ItemHandler struct {}
+
+func (h ItemHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+}
--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"github.com/alexdebril/feed-io-api-websocket/handler"
+	"log"
+	"net/http"
+)
+
 func main() {
-	
+	log.Println("starting server ...")
+	mux := http.NewServeMux()
+	h := &handler.ItemHandler{}
+
+	mux.Handle("/item", h)
+	s := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	err := s.ListenAndServe()
+	if err != nil {
+		log.Fatal("impossible to start the HTTP server")
+	}
 }
