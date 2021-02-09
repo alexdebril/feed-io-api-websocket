3d1e666544ef5b9585f3e20e2c77024bb6327488
Alex Debril <alexdebril@users.noreply.github.com>
2021-02-09 18:59:23 +0100

    1st POC of a working SSE server (but for 1 connection)

--- handler/handler.go
+++ handler/handler.go
@@ -1,44 +1,6 @@
 package handler
 
-import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-)
-
 type Item struct {
 	Title string `json:"title"`
 	FeedUrl string `json:"feed_url"`
-}
-
-type ItemHandler struct {
-	Message chan Item
-}
-
-func (h *ItemHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		w.WriteHeader(400)
-		return
-	}
-	item, err := h.getItem(r)
-	if err != nil {
-		fmt.Printf(err.Error())
-		w.WriteHeader(500)
-	}
-	log.Println("yo")
-	h.Message <- item
-	log.Println("yo man")
-	w.WriteHeader(204)
-}
-
-func (h *ItemHandler) getItem(r *http.Request) (Item, error) {
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		return Item{}, err
-	}
-	i := Item{}
-	err = json.Unmarshal(body, &i)
-	return i, err
 }
--- main.go
+++ main.go
@@ -1,26 +1,88 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/alexdebril/feed-io-api-websocket/handler"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+var ItemChannel chan handler.Item
+
 func main() {
 	log.Println("starting server ...")
 	mux := http.NewServeMux()
-	var msg chan handler.Item
-	h := &handler.ItemHandler{
-		Message: msg,
-	}
 
-	mux.Handle("/item", h)
+	mux.HandleFunc("/item", ReceiveMessage)
+	mux.HandleFunc("/read", publish)
+
 	s := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
+
 	err := s.ListenAndServe()
 	if err != nil {
 		log.Fatal("impossible to start the HTTP server")
+	}
+}
+
+func ReceiveMessage(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(400)
+		return
+	}
+	item, err := parseItem(r)
+	if err != nil {
+		fmt.Printf(err.Error())
+		w.WriteHeader(500)
+	}
+	ItemChannel <- item
+	w.WriteHeader(204)
+}
+
+func parseItem(r *http.Request) (handler.Item, error) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return handler.Item{}, err
+	}
+	i := handler.Item{}
+	err = json.Unmarshal(body, &i)
+	return i, err
+}
+
+func toJson(item handler.Item) string {
+	out, err := json.Marshal(item)
+	if err != nil {
+		return "{}"
+	}
+	return string(out)
+}
+
+func publish(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	ItemChannel = make(chan handler.Item)
+	defer func() {
+		close(ItemChannel)
+		ItemChannel = nil
+		log.Printf("client connection is closed")
+	}()
+
+	flusher, _ := w.(http.Flusher)
+
+	for {
+		select {
+		case item := <-ItemChannel:
+			_, _ = fmt.Fprintf(w, "%s\n", toJson(item))
+			flusher.Flush()
+		case <-r.Context().Done():
+			return
+		}
 	}
 }
